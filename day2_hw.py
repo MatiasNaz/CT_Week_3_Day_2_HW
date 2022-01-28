1d16b81a0943cc641da42e7455f07aa4c905b936
Matias Nazabal <mgnazabal@gmail.com>
2022-01-27 22:21:59 -0500

    redone hw

--- day2_hw.py
+++ day2_hw.py
@@ -3,8 +3,11 @@
 places = [" ", "Argentina", " ", "San Diego",
           "", "  ", "", "Boston", "New York"]
 
-
-def VisitPlaces(filter(lambda place: place True if places.strip()[0]))
+res = []
+for place in places:
+    if place.strip() != '':
+        res.append(place)
+print(res)
 
 
 #Exercise 2:
@@ -19,35 +22,27 @@ def SortFunct(name_list):
 
 print(SortFunct(author))
 
-
-#     #sort_list = sort(author.items(), key=lambda item: item[1])
-#     print(sort_tuples) [(1,1), (3,4), (2,9)]
-
-#     sort_list = {k: v for k, v in sort_tuples}
-
-# print(sort_list)
-
-
-# sorted_tuples = sorted(dict1.items(), key=lambda item: item[1])
-# print(sorted_tuples)  # [(1, 1), (3, 4), (2, 9)]
-# sorted_dict = {k: v for k, v in sorted_tuples}
-
-# print(sorted_dict)  # {1: 1, 3: 4, 2: 9}
-
-
 # Exercise 3:
-
 
 # F = (9/5)*C + 32
 places = [('Nashua', 32), ("Boston", 12), ("Los Angelos", 44), ("Miami", 29)]
 
-# x = list(places)
-# places = tuple(x)
+place_updated = list(map(lambda x: (x[0], ((x[-1]*1.8 + 32))), places))
 
-# print(places)
-places = list(map(lambda x: (x[0], ((x[-1])*1.8)+32, places))
-
-
-              print(places)
+print(place_updated)
 
 # Exercise 4
+
+def fibSeq(num):
+    if num <= 1:
+        return num
+    else:
+        return (fibSeq(num-2) + fibSeq(num-1))
+
+n_terms = 6
+
+if n_terms <= 0:
+    print("Only Positive Number")
+else:
+    for i in range(0, n_terms):
+        print(f"Iteration {i}:{fibSeq(i+1)}")
